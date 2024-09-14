c7785943b90242fa06d6e6837eb6a7a5a051729d
BeefyAmigo <alex.houser67@gmail.com>
2024-09-14 10:47:48 -0500

    Use math.Random to generate a random number 1-3
    I then uses else-if statements to print 'rock' if
    num is less than 2, 'paper' if greater than 2,
    and 'scissors' if num equals 2

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
-function getComputerChoice() {
-
+function getComputerChoice(min, max) {
+    return Math.floor(Math.random() * (3 - 1 + 1) + 1);
+}
+    const rndInt = getComputerChoice(1, 3);
+    
+    let num = rndInt
+    if (num < 2) {
+        console.log('rock');
+    } else if (num > 2) {
+        console.log('paper');
+    } else {
+        console.log('scissors')
     }
-
+     
