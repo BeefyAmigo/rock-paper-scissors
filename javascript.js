09e7182881b45a265f5bc247116c7e8afd254c36
BeefyAmigo <alex.houser67@gmail.com>
2024-09-18 14:30:38 -0500

    Remove rndInt + num variables
    I removed the rndInt + num variables from the
    getHumanChoice

--- javascript.js
+++ javascript.js
@@ -1,12 +1,9 @@
 function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (3 - 1 + 1) + 1);
 }
-    const rndInt = getComputerChoice(1, 3);
-    
-    let num = rndInt
-    if (num < 2) {
+    if (getComputerChoice() < 2) {
         console.log('rock');
-    } else if (num > 2) {
+    } else if (getComputerChoice() > 2) {
         console.log('paper');
     } else {
         console.log('scissors')
@@ -17,19 +14,17 @@ function getHumanChoice() {
    console.log(choice)
 }
 
-getHumanChoice(console.log)
+let humanScore = 0
+let computerScore = 0
 
-//let humanScore = 0
-//et computerScore = 0
+function playRound(humanChoice, computerChoice) {
+   humanChoice = humanChoice.toLowerCase();
+   // if (humanSelection === 'rock' && computerSelection === 'scissors') {
+   //     console.log("You Win! Rock beats scissors")
+   // }
+}
 
-//unction playRound(humanChoice, computerChoice) {
-   //humanChoice = humanChoice.toLowerCase();
-    //if (getHumanChoice === 'rock' && num === 'scissors') {
-    //    console.log("You Win! Rock beats scissors")
-    //}
-//}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-//const humanSelection = getHumanChoice;
-//const computerSelection = num;
-
-//layRound(humanSelection, computerSelection);
+//playRound(humanSelection, computerSelection);
