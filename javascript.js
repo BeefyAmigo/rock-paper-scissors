9235765901ae69829c6dd281632dc0ca12ffdde3
BeefyAmigo <alex.houser67@gmail.com>
2024-09-18 15:11:23 -0500

    Add variable rndInt to getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
 function getComputerChoice(min, max) {
-    return Math.floor(Math.random() * (3 - 1 + 1) + 1);
-}
-    if (getComputerChoice() < 2) {
+    let rndInt = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+    if (rndInt < 2) {
         console.log('rock');
-    } else if (getComputerChoice() > 2) {
+    } else if (rndInt > 2) {
         console.log('paper');
     } else {
         console.log('scissors')
     }
+}
 
 function getHumanChoice() {
    let choice = prompt("rock, paper, or scissors?");
@@ -18,13 +19,14 @@ let humanScore = 0
 let computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
-   humanChoice = humanChoice.toLowerCase();
-   // if (humanSelection === 'rock' && computerSelection === 'scissors') {
-   //     console.log("You Win! Rock beats scissors")
-   // }
+   //humanChoice = humanChoice.toLowerCase();
+    if (humanSelection === 'rock') {
+        console.log("You Win! Rock beats scissors");
+    }
+    
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-//playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
