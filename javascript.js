1d92ae9dae63371eac9d9e2f29133f82c1f51567
BeefyAmigo <alex.houser67@gmail.com>
2024-09-19 14:50:57 -0500

    Fix getComputerChoice() function
    I fixed the getComputerChoice by simplifying
    the random number generator to return the values
    instead on logging them to the console

--- javascript.js
+++ javascript.js
@@ -1,22 +1,27 @@
 function getComputerChoice() {
-    if (random(1, 3) < 2) {
-        console.log('rock');
-    } else if (random(1, 3) > 2) {
-        console.log('paper');
-    } else {
-        console.log('scissors')
-    }
+   const randomNumber = Math.floor(Math.random() * 3);
+
+   if (randomNumber === 0){
+    return 'rock'
+   }else if (randomNumber === 1){
+    return 'paper'
+   }else {
+    return 'scissors';
+   }
 }
+
 function random(min, max) {
-    return Math.floor(Math.random() * (3 - 1 +1)+1)
+    return Math.floor(Math.random() * (3 - 1 +1)+1);
 }
 
 function getHumanChoice() {
    let choice = prompt("rock, paper, or scissors?");
-   return (choice)
+
+   return (choice);
 }
-let humanScore = 0
-let computerScore = 0
+
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'scissors') {
@@ -26,16 +31,14 @@ function playRound(humanChoice, computerChoice) {
     }else if (humanChoice === 'paper' && computerChoice === 'rock') {
         console.log("You Win! Paper beats Rock");
     }else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        console.log("You Lose! Scissors beats Paper")
+        console.log("You Lose! Scissors beats Paper");
     }else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log("You Win! Scissors beats Paper")
+        console.log("You Win! Scissors beats Paper");
     }else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        console.log("You Lose! Rock beats Scissors")
+        console.log("You Lose! Rock beats Scissors");
     }else {
-        console.log("Its a Tie")
+        console.log("Its a Tie");
     }
-    
-    
 }
 
 const humanSelection = getHumanChoice();
