55184a730ca95f7807700cd4920abf990baabf1c
BeefyAmigo <alex.houser67@gmail.com>
2024-09-19 12:10:35 -0500

    Add new random() function
    remove the random number variable from the
    getComputerChoice and replace it with its own
    random() function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
-function getComputerChoice(min, max) {
-    let rndInt = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-
-    if (rndInt < 2) {
+function getComputerChoice() {
+    if (random(1, 3) < 2) {
         console.log('rock');
-    } else if (rndInt > 2) {
+    } else if (random(1, 3) > 2) {
         console.log('paper');
     } else {
         console.log('scissors')
     }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (3 - 1 +1)+1)
 }
 
 function getHumanChoice() {
@@ -19,7 +21,6 @@ let humanScore = 0
 let computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
-   //humanChoice = humanChoice.toLowerCase();
     if (humanSelection === 'rock') {
         console.log("You Win! Rock beats scissors");
     }
