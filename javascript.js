07827babe5eb8ad69c44589dfad2e83c1f515fa0
BeefyAmigo <alex.houser67@gmail.com>
2024-09-19 12:51:38 -0500

    Add if else statements to the playRound function
    I added if else statements for displaying a
    message depending on if you win or lose

--- javascript.js
+++ javascript.js
@@ -7,23 +7,34 @@ function getComputerChoice() {
         console.log('scissors')
     }
 }
-
 function random(min, max) {
     return Math.floor(Math.random() * (3 - 1 +1)+1)
 }
 
 function getHumanChoice() {
    let choice = prompt("rock, paper, or scissors?");
-   console.log(choice)
+   return (choice)
 }
-
 let humanScore = 0
 let computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
-    if (humanSelection === 'rock') {
-        console.log("You Win! Rock beats scissors");
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log("You Win! Rock beats Scissors");
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log("You Lose! Paper beats Rock");
+    }else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log("You Win! Paper beats Rock");
+    }else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log("You Lose! Scissors beats Paper")
+    }else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log("You Win! Scissors beats Paper")
+    }else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log("You Lose! Rock beats Scissors")
+    }else {
+        console.log("Its a Tie")
     }
+    
     
 }
 
